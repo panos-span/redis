383fa511bee40294be32878ee4cf6195b100f8b5
Panagiotis Spanakis <t8200158@aueb.gr>
2023-05-04 20:41:33 +0300

    Redis v2 (Needs work, but it's in a good path)

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import threading
-
-import redis
 import json
+import random
+import threading
 import time
 from datetime import datetime
-import random
-from objects import User, MeetingInstance
-from mysql_handler import MySQLHandler
-import pandas as pd
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+import pandas as pd
+import redis
+
+from mysql_handler import MySQLHandler
+from objects import User, Meeting
 
 
 # 1
-def join_meeting(user, meeting_instance, audience=None):
+def join_meeting(user, meeting, audience=None):
+    # Get meeting instance status from redis
+    key = f'meeting_{meeting.meetingID}'
+    if r.hget(key, 'isActive') is None:
+        return False
+    meeting_status = int(r.hget(key, 'isActive').decode('utf-8'))
+    if not meeting_status:
+        return False
     if audience is None or user.email in audience:
-        r.sadd(f'participants_{meeting_instance.meetingID}_{meeting_instance.orderID}', user.userID)
+        r.sadd(f'participants_{meeting.meetingID}', user.userID)
         event = {
             'event_id': f'event_{(time.time())}',
             'user_id': user.userID,
             'event_type': 1,  # join_meeting
             'timestamp': datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         }
+        print(event)
         r.rpush('events_log', json.dumps(event, default=str))
         return True
     return False
 
 
 # 2
-def leave_meeting(user_id, meetingID, meeting_instance_orderID):
-    if r.srem(f'participants_{meetingID}_{meeting_instance_orderID}', user_id):
+def leave_meeting(user_id, meetingID):
+    key = f'meeting_{meetingID}'
+    if r.hget(key, 'isActive') is None:
+        return False
+    meeting_status = int(r.hget(key, 'isActive').decode('utf-8'))
+    if not meeting_status:
+        return False
+    if r.srem(f'participants_{meetingID}', user_id):
         event = {
             'event_id': f'event_{(time.time())}',
             'user_id': user_id,
@@ -42,65 +55,79 @@ def leave_meeting(user_id, meetingID, meeting_instance_orderID):
 
 
 # 3
-def get_current_participants(meetingID, meeting_instance_orderID):
-    return list(r.smembers(f'participants_{meetingID}_{meeting_instance_orderID}'))
+def get_current_participants(meetingID):
+    return list(r.smembers(f'participants_{meetingID}'))
 
 
 # 4
-def get_active_meetings_instances():
+def get_active_meetings():
     active_meetings = {}
-    for key in r.scan_iter('meeting_instance_*_*'):
+    for key in r.scan_iter('meeting_*'):
         # meeting_instance = json.loads(r.get(key))
         hash_table_contents = r.hgetall(key.decode("utf-8"))
         # Decode the byte strings returned by the hgetall method
-        meeting_instance = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
-        if str2datetime(meeting_instance['fromDatetime']) <= datetime.now() <= str2datetime(
-                meeting_instance['toDatetime']):
-            active_meetings[f"{meeting_instance['meetingID']}_{meeting_instance['orderID']}"] = meeting_instance
+        meeting = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
+        print(meeting)
+        if meeting['isActive']:
+            active_meetings[meeting['meetingID']] = meeting
     return active_meetings
 
 
 # 5
-def end_meeting(meetingID, meeting_instance_orderID):
-    participants = r.smembers(f'participants_{meetingID}_{meeting_instance_orderID}')
+def end_meeting(meetingID):
+    participants = r.smembers(f'participants_{meetingID}')
     for user_id in participants:
-        leave_meeting(user_id.decode('utf-8'), meetingID, meeting_instance_orderID)
-    r.delete(f'meeting_instance_{meetingID}_{meeting_instance_orderID}')
+        leave_meeting(user_id.decode('utf-8'), meetingID)
+    r.delete(f'meeting_{meetingID}')
 
 
 # 6
-def post_chat_message(user_id, meetingID, meeting_instance_orderID, message):
+def post_chat_message(user_id, meetingID, message):
     chat_message = {
         'user_id': user_id,
         'message': message,
         'timestamp': datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     }
-    r.rpush(f'chat_{meetingID}_{meeting_instance_orderID}', json.dumps(chat_message, default=str))
+    r.rpush(f'chat_{meetingID}', json.dumps(chat_message, default=str))
 
 
 # 7
-def get_chat_messages(meetingID, meeting_instance_orderID):
+def get_chat_messages(meetingID):
     messages = []
-    for message in r.lrange(f'chat_{meetingID}_{meeting_instance_orderID}', 0, -1):
+    for message in r.lrange(f'chat_{meetingID}', 0, -1):
         messages.append(json.loads(message))
     return messages
 
 
-# 8
-def get_join_timestamps(meetingID, meeting_instance_orderID):
+def get_join_timestamps(meetingID):
     join_timestamps = {}
     for event in r.lrange('events_log', 0, -1):
         event_data = json.loads(event)
         if event_data['event_type'] == 1 and event_data['user_id'] in r.smembers(
-                f'participants_{meetingID}_{meeting_instance_orderID}'):
+                f'participants_{meetingID}'):
             join_timestamps[event_data['user_id']] = event_data['timestamp']
     return join_timestamps
 
 
+# 8
+def get_active_meeting_timestamps():
+    active_meetings = get_active_meetings()
+    active_meeting_timestamps = {}
+    for meetingID in active_meetings:
+        active_meeting_timestamps[meetingID] = get_join_timestamps(meetingID)
+    return active_meeting_timestamps
+
+
 # 9
-def get_user_chat_messages(meetingID, meeting_instance_orderID, user_id):
+def get_user_chat_messages(meetingID, user_id):
+    key = f'meeting_{meetingID}'
+    if r.hget(key, 'isActive') is None:
+        return False
+    meeting_status = int(r.hget(key, 'isActive').decode('utf-8'))
+    if not meeting_status:
+        return False
     user_messages = []
-    for message in r.lrange(f'chat_{meetingID}_{meeting_instance_orderID}', 0, -1):
+    for message in r.lrange(f'chat_{meetingID}', 0, -1):
         message_data = json.loads(message)
         if message_data['user_id'] == user_id:
             user_messages.append(message_data)
@@ -108,28 +135,43 @@ def get_user_chat_messages(meetingID, meeting_instance_orderID, user_id):
 
 
 def generateRandomMessage():
-    return "Random Message " + str(random.randint(-10000, 1000000))
+    return "Random Message " + str(datetime.now())
 
 
 def controller():
     start_time = time.time()
     while main.is_alive():
         print(time.time() - start_time)
-        # Check for active meetings if not active then deactivate them
-        active_meetings_instances = get_active_meetings_instances()
-        for key in r.scan_iter('meeting_instance_*_*'):
+        for key in r.scan_iter('meeting_*'):
             # meeting_instance = json.loads(r.get(key))
             hash_table_contents = r.hgetall(key.decode("utf-8"))
             # Decode the byte strings returned by the hgetall method
-            meeting_instance = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
-
+            meeting = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
+            print(meeting)
+            print(meeting['meetingID'])
+            meeting_inst = json.loads(
+                meeting_instances[meeting_instances['meetingID'] == int(meeting['meetingID'])].
+                to_json(orient='records', date_format='iso'))
+            isOnceActive = False
+            for meeting_instance in meeting_inst:
+                # Activate meeting if it is not active
+                print("!"*50)
+                print(str2datetime(meeting_instance['fromDatetime']))
+                current_datetime = datetime.now()
+                formatted_datetime = str(current_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z')
+                print(formatted_datetime)
+                print(str2datetime(meeting_instance['toDatetime']))
+                print("!"*50)
+                if str2datetime(meeting_instance['fromDatetime']) <= str2datetime(formatted_datetime) <= str2datetime(
+                        meeting_instance['toDatetime']):
+                    r.hset(f'meeting_{meeting_instance["meetingID"]}', 'isActive',
+                           1)
+                    isOnceActive = True
+                    break
             # Deactivate meeting if it is not active
-            if str2datetime(meeting_instance['toDatetime']) < datetime.now():
-                end_meeting(meeting_instance['meetingID'], meeting_instance['orderID'])
-                # del active_meetings_instances[f"{meeting_instance['meetingID']}_{meeting_instance['orderID']}"]
-            # Activate meeting if it is not active
-            # elif str2datetime(meeting_instance['fromDatetime']) <= datetime.now() <= str2datetime(
-            #        meeting_instance['toDatetime']):
+            if not isOnceActive:
+                r.hset(f'meeting_{meeting["meetingID"]}', 'isActive',
+                       0)
 
         time.sleep(60)
 
@@ -149,36 +191,36 @@ def initialise_columns():
 def run():
     start_time = time.time()
     while time.time() - start_time < 120:  # Run for 2 minutes
-        print(time.time() - start_time)
+        # print(time.time() - start_time)
         choice = random.randint(0, 8)
         user_num = random.randint(0, 3)
-        meeting_num = random.randint(0, 3)
+        meeting_num = random.randint(1, 4)
         user = User(**users[user_num])
-        meeting_instance = MeetingInstance(**meeting_instances[meeting_num])
+        meeting = Meeting(**meetings[meeting_num])
         if choice == 0:
-            if meetings[meeting_instance.meetingID]['is_public']:
-                join_meeting(user, meeting_instance)
+            if meetings[meeting.meetingID]['is_public']:
+                join_meeting(user, meeting)
             else:
                 print('Meeting is private')
-                print(join_meeting(user, meeting_instance,
-                                   meeting_audience[meeting_audience['meetingID'] == meeting_num + 1]['email'].values))
+                print(join_meeting(user, meeting,
+                                   meeting_audience[meeting_audience['meetingID'] == meeting_num]['email'].values))
         elif choice == 1:
-            leave_meeting(user.userID, meeting_instance.meetingID, meeting_instance.orderID)
+            leave_meeting(user.userID, meeting.meetingID)
         elif choice == 2:
-            get_current_participants(meeting_instance.meetingID, meeting_instance.orderID)
+            get_current_participants(meeting.meetingID)
         elif choice == 3:
-            get_active_meetings_instances()
+            get_active_meetings()
         elif choice == 4:
-            end_meeting(meeting_instance.meetingID, meeting_instance.orderID)
+            end_meeting(meeting.meetingID)
         elif choice == 5:
-            post_chat_message(user.userID, meeting_instance.meetingID, meeting_instance.orderID,
+            post_chat_message(user.userID, meeting.meetingID,
                               generateRandomMessage())
         elif choice == 6:
-            get_chat_messages(meeting_instance.meetingID, meeting_instance.orderID)
+            get_chat_messages(meeting.meetingID)
         elif choice == 7:
-            get_join_timestamps(meeting_instance.meetingID, meeting_instance.orderID)
+            get_join_timestamps(meeting.meetingID)
         elif choice == 8:
-            get_user_chat_messages(meeting_instance.meetingID, meeting_instance.orderID, user.userID)
+            get_user_chat_messages(meeting.meetingID, user.userID)
         time.sleep(1)  # Sleep for 1 second
 
 
@@ -190,20 +232,21 @@ if __name__ == '__main__':
     users = pd.DataFrame(db.select('SELECT * FROM users'), columns=user_columns)
     meetings = pd.DataFrame(db.select('SELECT * FROM meetings'), columns=meeting_columns)
     meeting_instances = pd.DataFrame(db.select('SELECT * FROM meeting_instances'), columns=meeting_instance_columns)
-
     meeting_audience = pd.DataFrame(db.select('SELECT * FROM meeting_audience'), columns=meeting_audience_columns)
     users = json.loads(users.to_json(orient='records'))
     meetings = json.loads(meetings.to_json(orient='records'))
-    meeting_instances = json.loads(meeting_instances.to_json(orient='records', date_format='iso'))
-    # Insert active meeting_instances to redis
+
+    # Insert meetings to redis
     with r.pipeline() as pipe:
-        for meeting_instance in meeting_instances:
-            # if str2datetime(meeting_instance['fromDatetime']) <= datetime.now() <= str2datetime(
-            #        meeting_instance['toDatetime']):
-            for field, value in meeting_instance.items():
-                pipe.hset(f'meeting_instance_{meeting_instance["meetingID"]}_{meeting_instance["orderID"]}', field,
-                          value)
+        for meeting in meetings:
+            for field, value in meeting.items():
+                pipe.hset(f'meeting_{meeting["meetingID"]}', field, value)
         pipe.execute()
+
+    meetings = {entry['meetingID']: entry for entry in meetings}
+    user = User(**users[0])
+    meeting = Meeting(**meetings[1])
+    join_meeting(user, meeting)
 
     main = threading.Thread(target=run)
     main.start()
@@ -218,15 +261,14 @@ if __name__ == '__main__':
     scheduler.join(timeout=1)  # Give it a chance to finish the current iteration
 
     # Load the event_log data from redis to mysql
-    while r.llen('events_log') > 0:
-        # Pop a JSON document from the Redis list
-        json_document = r.lpop('event_log').decode("utf-8")
-        event_log = json.loads(json_document)
-        sql_query = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES (%s, %s, %s, %s)"
-        values = (
-            event_log['event_id'], event_log['user_id'], event_log['event_type'],
-            str2datetime(event_log['timestamp']))
-        # values = **event_log
-        db.execute(sql_query, values)
+    #while r.llen('events_log') > 0:
+    #    # Pop a JSON document from the Redis list
+    #    json_document = r.lpop('events_log').decode("utf-8")
+    #    event_log = json.loads(json_document)
+    #    sql_query = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES (%s, %s, %s, %s)"
+    #    values = (
+    #        event_log['event_id'], event_log['user_id'], event_log['event_type'],
+    #        str2datetime(event_log['timestamp']))
+    #    db.execute(sql_query, values)
 
     db.close()
--- objects.py
+++ objects.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-
 class User:
     def __init__(self, **kwargs):
         self.userID = kwargs.get('userID')
@@ -16,42 +13,10 @@ class User:
 class Meeting:
 
     def __init__(self, **kwargs):
-        self.id = kwargs.get('meetingID')
+        self.meetingID = kwargs.get('meetingID')
         self.title = kwargs.get('title')
         self.description = kwargs.get('description')
         self.isPublic = kwargs.get('isPublic')
-
-    def __getattr__(self, item):
-        return self.__dict__[item]
-
-
-class MeetingInstance:
-
-    def __init__(self, **kwargs):
-        self.isActive = None
-        self.meetingID: int = kwargs.get('meetingID')
-        self.orderID: int = kwargs.get('orderID')
-        self.fromDatetime: datetime = self.str2datetime(kwargs.get('fromDatetime'))
-        self.toDatetime: datetime = self.str2datetime(kwargs.get('toDatetime'))
-        self.checkActivity()
-
-    def str2datetime(self, date_str):
-        return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
-
-    def __getattr__(self, item):
-        return self.__dict__[item]
-
-    def checkActivity(self):
-        self.isActive = self.fromDatetime < datetime.now() < self.toDatetime
-
-
-class EventLog:
-
-    def __init__(self, **kwargs):
-        self.id = kwargs.get('event_id')
-        self.user_id = kwargs.get('userID')
-        self.event_type = kwargs.get('event_type')  # 1 if user joined, 2 if user left , 3 if user timed out
-        self.timestamp = kwargs.get('timestamp')
 
     def __getattr__(self, item):
         return self.__dict__[item]
