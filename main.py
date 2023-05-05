6796fa4fa703d0de5595b2cfb51997c1077a4fd1
Panagiotis Spanakis <t8200158@aueb.gr>
2023-05-06 00:58:12 +0300

    Redis v2 Done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+This program is designed to manage meetings using Redis and MySQL databases.
+It allows users to join and leave meetings, view the participants in a meeting,
+view active meetings, end meetings, send chat messages, and view chat messages.
+The program simulates these actions using random selections and user inputs.
+The main logic of the program is broken down into several functions,
+and a controller function is used to manage the state of active meetings.
+"""
+
+__authors__ = 'Panagiotis Spanakis, Emmanouil Dellatolas'
+
 import json
 import random
 import threading
@@ -13,6 +24,15 @@ from objects import User, Meeting
 
 # 1
 def join_meeting(user, meeting, audience=None):
+    """
+    Allows a user to join a meeting if the meeting is active and the user is allowed to join.
+
+    :param user: the user that wants to join the meeting
+    :param meeting: the meeting that the user wants to join
+    :param audience: the list of users that are allowed to join the meeting
+    :return: true if the user joined the meeting, false otherwise
+    """
+
     # Get meeting instance status from redis
     key = f'meeting_{meeting.meetingID}'
     if r.hget(key, 'isActive') is None:
@@ -20,6 +40,7 @@ def join_meeting(user, meeting, audience=None):
     meeting_status = int(r.hget(key, 'isActive').decode('utf-8'))
     if not meeting_status:
         return False
+    # If the user is allowed to join, add them to the participants list
     if audience is None or user.email in audience:
         r.sadd(f'participants_{meeting.meetingID}', user.userID)
         event = {
@@ -28,14 +49,21 @@ def join_meeting(user, meeting, audience=None):
             'event_type': 1,  # join_meeting
             'timestamp': datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         }
-        print(event)
         r.rpush('events_log', json.dumps(event, default=str))
         return True
     return False
 
 
 # 2
-def leave_meeting(user_id, meetingID):
+def leave_meeting(user_id, meetingID, timeOut=False):
+    """
+    Allows a user to leave a meeting if the meeting is active and the user is part of the participants.
+
+    :param user_id: the id of the user that wants to leave the meeting
+    :param meetingID: the id of the meeting that the user wants to leave
+    :param timeOut: true if the user left the meeting due to a timeout, false otherwise
+    :return: true if the user left the meeting, false otherwise
+    """
     key = f'meeting_{meetingID}'
     if r.hget(key, 'isActive') is None:
         return False
@@ -43,10 +71,15 @@ def leave_meeting(user_id, meetingID):
     if not meeting_status:
         return False
     if r.srem(f'participants_{meetingID}', user_id):
+        # Check if the user left the meeting due to a timeout or not
+        if timeOut:
+            event_type = 3  # timeout
+        else:
+            event_type = 2  # leave_meeting
         event = {
             'event_id': f'event_{(time.time())}',
             'user_id': user_id,
-            'event_type': 2,  # leave_meeting
+            'event_type': event_type,
             'timestamp': datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         }
         r.rpush('events_log', json.dumps(event, default=str))
@@ -56,18 +89,27 @@ def leave_meeting(user_id, meetingID):
 
 # 3
 def get_current_participants(meetingID):
+    """
+    Retrieves a list of the current participants in a meeting.
+
+    :param meetingID: the id of the meeting
+    :return: a list of the current participants in a meeting
+    """
     return list(r.smembers(f'participants_{meetingID}'))
 
 
 # 4
 def get_active_meetings():
+    """
+    Retrieves a list of active meetings.
+    :return: A list of active meetings.
+    """
     active_meetings = {}
     for key in r.scan_iter('meeting_*'):
         # meeting_instance = json.loads(r.get(key))
         hash_table_contents = r.hgetall(key.decode("utf-8"))
         # Decode the byte strings returned by the hgetall method
         meeting = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
-        print(meeting)
         if meeting['isActive']:
             active_meetings[meeting['meetingID']] = meeting
     return active_meetings
@@ -75,14 +117,29 @@ def get_active_meetings():
 
 # 5
 def end_meeting(meetingID):
+    """
+    Ends a meeting by removing all participants from the meeting.
+
+    :param meetingID:
+    :return:
+    """
     participants = r.smembers(f'participants_{meetingID}')
     for user_id in participants:
-        leave_meeting(user_id.decode('utf-8'), meetingID)
-    r.delete(f'meeting_{meetingID}')
+        leave_meeting(user_id.decode('utf-8'), meetingID, True)
+    # Clear the participants
+    r.delete(f'participants_{meetingID}')
 
 
 # 6
 def post_chat_message(user_id, meetingID, message):
+    """
+    Posts a chat message to a meeting.
+
+    :param user_id: the id of the user that posted the message
+    :param meetingID: the id of the meeting that the message was posted to
+    :param message: the message that was posted
+    :return: nothing
+    """
     chat_message = {
         'user_id': user_id,
         'message': message,
@@ -93,6 +150,11 @@ def post_chat_message(user_id, meetingID, message):
 
 # 7
 def get_chat_messages(meetingID):
+    """
+    Retrieves all chat messages from a meeting.
+    :param meetingID: the id of the meeting
+    :return: list of chat messages
+    """
     messages = []
     for message in r.lrange(f'chat_{meetingID}', 0, -1):
         messages.append(json.loads(message))
@@ -100,6 +162,11 @@ def get_chat_messages(meetingID):
 
 
 def get_join_timestamps(meetingID):
+    """
+    Retrieves the join timestamps of all participants in a meeting.
+    :param meetingID: the id of the meeting
+    :return: dictionary of join timestamps
+    """
     join_timestamps = {}
     for event in r.lrange('events_log', 0, -1):
         event_data = json.loads(event)
@@ -111,6 +178,11 @@ def get_join_timestamps(meetingID):
 
 # 8
 def get_active_meeting_timestamps():
+    """
+    Retrieves the join timestamps of all participants in all active meetings.
+
+    :return: dictionary of join timestamps
+    """
     active_meetings = get_active_meetings()
     active_meeting_timestamps = {}
     for meetingID in active_meetings:
@@ -120,6 +192,12 @@ def get_active_meeting_timestamps():
 
 # 9
 def get_user_chat_messages(meetingID, user_id):
+    """
+    Retrieves all chat messages from a meeting for a specific user.
+    :param meetingID: the id of the meeting
+    :param user_id: the id of the user
+    :return: list of chat messages
+    """
     key = f'meeting_{meetingID}'
     if r.hget(key, 'isActive') is None:
         return False
@@ -135,60 +213,74 @@ def get_user_chat_messages(meetingID, user_id):
 
 
 def generateRandomMessage():
+    """
+    Generates a random message for testing purposes.
+    :return: random message
+    """
     return "Random Message " + str(datetime.now())
 
 
 def controller():
+    """
+    Controller method that runs every 60 seconds and checks for active meetings.
+    In the start of the program this method instantiates the field isActive (True/False)
+    for all the meetings in the redis database.
+
+    :return: nothing
+    """
     start_time = time.time()
     while main.is_alive():
         print(time.time() - start_time)
         for key in r.scan_iter('meeting_*'):
-            # meeting_instance = json.loads(r.get(key))
             hash_table_contents = r.hgetall(key.decode("utf-8"))
             # Decode the byte strings returned by the hgetall method
             meeting = {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_table_contents.items()}
-            print(meeting)
-            print(meeting['meetingID'])
-            meeting_inst = json.loads(
-                meeting_instances[meeting_instances['meetingID'] == int(meeting['meetingID'])].
-                to_json(orient='records', date_format='iso'))
-            isOnceActive = False
-            for meeting_instance in meeting_inst:
-                # Activate meeting if it is not active
-                print("!"*50)
-                print(str2datetime(meeting_instance['fromDatetime']))
-                current_datetime = datetime.now()
-                formatted_datetime = str(current_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z')
-                print(formatted_datetime)
-                print(str2datetime(meeting_instance['toDatetime']))
-                print("!"*50)
-                if str2datetime(meeting_instance['fromDatetime']) <= str2datetime(formatted_datetime) <= str2datetime(
-                        meeting_instance['toDatetime']):
-                    r.hset(f'meeting_{meeting_instance["meetingID"]}', 'isActive',
-                           1)
-                    isOnceActive = True
-                    break
-            # Deactivate meeting if it is not active
-            if not isOnceActive:
-                r.hset(f'meeting_{meeting["meetingID"]}', 'isActive',
-                       0)
+            meeting_inst = meeting_instances[meeting_instances['meetingID'] == int(meeting['meetingID'])]
+            # Check for meeting instances where the current time is between the start and end time
+            meeting_inst_active = meeting_inst[
+                (meeting_inst['fromDatetime'] <= datetime.now()) & (meeting_inst['toDatetime'] >= datetime.now())]
+            meeting_inst_active = json.loads(meeting_inst_active.to_json(orient='records', date_format='iso'))[0]
 
+            if meeting_inst_active is not None:
+                r.hset(f'meeting_{meeting_inst_active["meetingID"]}', 'isActive',
+                       1)
+                continue
+            # If there are no active meeting instances, make the meeting inactive
+            meetingID = int(meeting['meetingID'])
+            r.hset(f'meeting_{meetingID}', 'isActive',
+                   0)
         time.sleep(60)
 
 
 def str2datetime(date_str):
+    """
+    Converts a string to a datetime object.
+    :param date_str: the string to be converted
+    :return: datetime object
+    """
     return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
 
 
 def initialise_columns():
-    user_columns = ['userID', 'username', 'age', 'gender', 'email']
-    meeting_columns = ['meetingID', 'title', 'description', 'is_public']
+    """
+    Initialises the columns of the dataframes.
+    :return: dataframe columns
+    """
+    user_columns = ['userID', 'email']
+    meeting_columns = ['meetingID', 'is_public']
     meeting_audience_columns = ['meetingID', 'email']
     meeting_instance_columns = ['meetingID', 'orderID', 'fromDatetime', 'toDatetime']
     return user_columns, meeting_columns, meeting_audience_columns, meeting_instance_columns
 
 
 def run():
+    """
+    Runs the program.
+
+    :return: nothing
+    """
+    # Wait for the controller to run first
+    time.sleep(1)
     start_time = time.time()
     while time.time() - start_time < 120:  # Run for 2 minutes
         # print(time.time() - start_time)
@@ -224,15 +316,45 @@ def run():
         time.sleep(1)  # Sleep for 1 second
 
 
+def migrateEventsLog():
+    """
+    Migrates the events_log from redis to mysql.
+
+    :return: nothing
+    """
+    # Load the event_log data from redis to mysql
+    while r.llen('events_log') > 0:
+        # Pop a JSON document from the Redis list
+        json_document = r.lpop('events_log').decode("utf-8")
+        event_log = json.loads(json_document)
+        sql_query = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES (%s, %s, %s, %s)"
+        values = (
+            event_log['event_id'], event_log['user_id'], event_log['event_type'],
+            str2datetime(event_log['timestamp']))
+        db.execute(sql_query, values)
+
+
 if __name__ == '__main__':
+    """
+    Main method.
+    """
+    # Connect to redis
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    # Load dataframe columns
     user_columns, meeting_columns, meeting_audience_columns, meeting_instance_columns = initialise_columns()
+
+    # Connect to mysql
     db = MySQLHandler()
     db.connect()
-    users = pd.DataFrame(db.select('SELECT * FROM users'), columns=user_columns)
-    meetings = pd.DataFrame(db.select('SELECT * FROM meetings'), columns=meeting_columns)
+
+    # Load data from mysql to pandas dataframes
+    users = pd.DataFrame(db.select('SELECT userID,email FROM users'), columns=user_columns)
+    meetings = pd.DataFrame(db.select('SELECT meetingID,isPublic FROM meetings'), columns=meeting_columns)
     meeting_instances = pd.DataFrame(db.select('SELECT * FROM meeting_instances'), columns=meeting_instance_columns)
     meeting_audience = pd.DataFrame(db.select('SELECT * FROM meeting_audience'), columns=meeting_audience_columns)
+
+    # Convert the dataframes to json
     users = json.loads(users.to_json(orient='records'))
     meetings = json.loads(meetings.to_json(orient='records'))
 
@@ -243,32 +365,29 @@ if __name__ == '__main__':
                 pipe.hset(f'meeting_{meeting["meetingID"]}', field, value)
         pipe.execute()
 
-    meetings = {entry['meetingID']: entry for entry in meetings}
-    user = User(**users[0])
-    meeting = Meeting(**meetings[1])
-    join_meeting(user, meeting)
+    # Make keys of the meetings dictionary the meetingID for faster access
+    meetings = {meeting['meetingID']: meeting for meeting in meetings}
 
+    # Start the continuous thread
     main = threading.Thread(target=run)
     main.start()
 
+    # Start the periodic thread (scheduler)
     scheduler = threading.Thread(target=controller)
     scheduler.start()
 
     # Wait for the continuous thread to finish
     main.join()
 
-    # Terminate the periodic thread after the continuous thread is done
+    # Terminate the scheduler after the continuous thread is done
     scheduler.join(timeout=1)  # Give it a chance to finish the current iteration
 
-    # Load the event_log data from redis to mysql
-    #while r.llen('events_log') > 0:
-    #    # Pop a JSON document from the Redis list
-    #    json_document = r.lpop('events_log').decode("utf-8")
-    #    event_log = json.loads(json_document)
-    #    sql_query = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES (%s, %s, %s, %s)"
-    #    values = (
-    #        event_log['event_id'], event_log['user_id'], event_log['event_type'],
-    #        str2datetime(event_log['timestamp']))
-    #    db.execute(sql_query, values)
+    # Migrate the events_log from redis to mysql
+    migrateEventsLog()
 
+    # Clear the participants sets if they exist
+    for key in r.scan_iter('participants_*'):
+        r.delete(key.decode("utf-8"))
+
+    # Close the connection to mysql
     db.close()
--- objects.py
+++ objects.py
@@ -1,8 +1,19 @@
 class User:
+    """
+    User object
+
+    Attributes:
+        userID: int
+        name: str
+        age: int
+        gender: str
+        email: str
+    """
+
     def __init__(self, **kwargs):
-        self.userID = kwargs.get('userID')
+        self.userID: int = kwargs.get('userID')
         self.name = kwargs.get('username')
-        self.age = kwargs.get('age')
+        self.age: int = kwargs.get('age')
         self.gender = kwargs.get('gender')
         self.email = kwargs.get('email')
 
@@ -11,6 +22,15 @@ class User:
 
 
 class Meeting:
+    """
+    Meeting object
+
+    Attributes:
+        meetingID: int
+        title: str
+        description: str
+        isPublic: bool
+    """
 
     def __init__(self, **kwargs):
         self.meetingID = kwargs.get('meetingID')
